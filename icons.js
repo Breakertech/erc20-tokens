aedb14009923c7f31dafb8b75f120da853b2e16b
Victor Davydov <septembermd@gmail.com>
2018-08-02 23:26:46 +0300

    Add icons

--- icons.js
+++ icons.js
@@ -1,11 +1,164 @@
-const icons = {};
+import adt from './icons/adt';
+import ant from './icons/ant';
+import ast from './icons/ast';
+import bat from './icons/bat';
+import bcap from './icons/bcap';
+import bnt from './icons/bnt';
+import cfi from './icons/cfi';
+import cvc from './icons/cvc';
+import dgd from './icons/dgd';
+import dnt from './icons/dnt';
+import edg from './icons/edg';
+import eos from './icons/eos';
+import eth from './icons/eth';
+import ethos from './icons/ethos';
+import fun from './icons/fun';
+import gno from './icons/gno';
+import gnt from './icons/gnt';
+import grmtk from './icons/grmtk';
+import guppy from './icons/guppy';
+import happy from './icons/happy';
+import hmq from './icons/hmq';
+import icn from './icons/icn';
+import knc from './icons/knc';
+import lun from './icons/lun';
+import mana from './icons/mana';
+import mco from './icons/mco';
+import mkr from './icons/mkr';
+import mln from './icons/mln';
+import mtl from './icons/mtl';
+import net from './icons/net';
+import nmr from './icons/nmr';
+import omg from './icons/omg';
+import pay from './icons/pay';
+import plu from './icons/plu';
+import qtum from './icons/qtum';
+import rep from './icons/rep';
+import rlc from './icons/rlc';
+import rol from './icons/rol';
+import san from './icons/san';
+import sngls from './icons/sngls';
+import sngx from './icons/sngx';
+import snt from './icons/snt';
+import st from './icons/st';
+import storj from './icons/storj';
+import swt from './icons/swt';
+import taas from './icons/taas';
+import time from './icons/time';
+import tkn from './icons/tkn';
+import trst from './icons/trst';
+import vslice from './icons/vslice';
+import wings from './icons/wings';
+import xaurum from './icons/xaurum';
+import zrx from './icons/zrx';
 
-/* eslint-disable max-len */
-icons['0xaec2e87e0a235266d9c5adc9deb4b2e29b54d009'] = `
-<svg viewBox="0 0 28 19">
-  <path d="M14.0079045 18.370666l-1.3406994-2.6409278c-2.9489257-.2697284-5.838484-1.3880821-8.5702946-3.0842602-1.1055423-.68643-2.1090436-1.4217356-2.9923973-2.1573395-.5312286-.4423753-.9054341-.7876489-1.1043014-.9873151L0 9.1354128c.1983773-.1996367.5716723-.5448512 1.1016371-.9871481.8812497-.7354715 1.8824276-1.4706448 2.9854951-2.1569528 2.6382167-1.641449 5.428489-2.7432725 8.2806675-3.0563092l.8447509-1.5616966L13.9944208 0l.2248861.3966407.9454064 1.6675287.4670206.8575785c2.8523407.3129677 5.6427734 1.414822 8.2811339 3.0563604 1.1030675.686308 2.1042454 1.4214813 2.9854951 2.1569528.5299648.4422969.9032598.7875114 1.1016371.9871481l-.0002118.3654106c-.1988673.1996662-.5730728.5449398-1.1043014.9873151-.8833537.7356039-1.886855 1.4709095-2.9923973 2.1573395-2.7314727 1.6959682-5.620654 2.8142551-8.5692001 3.0841601l-1.3259849 2.6542315zM2.3987307 9.7043678c.7219444.4640798 1.5333554.944333 2.4070134 1.410155 2.3075362 1.2303454 4.6183711 2.1372442 6.7903835 2.5482071L9.2036021 8.9197436l.0039069-.2407874.9532004-1.7463296.220272.449239.6416106 1.3085586 2.5089304 5.1957611c.0637431.0022701.1315592.0036915.206303.0044596.1211303.0008302.1211303.0008302.2440351.0008314.1519704-.000003.2166426-.0002685.2980398-.0016151.0485631-.0008033.0945826-.0019695.1390295-.0035678l1.810324-3.8127682 1.5761116-3.1497899.9508467 1.7420173.0039068.2407874-2.3921399 4.7422218c4.0083768-.7855719 10.0161881-3.9603147 10.0161881-4.3439538 0-.3369613-5.7104486-3.3913869-9.6044645-4.2593209l.5262566.9812191.006087.2327823-.8786403 1.866341-1.9060854-3.3763938c-.2375156-.0128236-.3875814-.0180923-.54546-.0180923-.1589652 0-.2966473.0047774-.5590595.0185023l-1.9273741 3.3525-.8666562-1.78736.0060654-.2377864.5674779-1.032658c-1.8150991.388253-3.7232379 1.1106341-5.6492392 2.0738458-1.0496695.5249498-2.0364312 1.0875713-2.9308124 1.6500518-.3376034.2123205-.6153704.3946464-.8259751.5375156.1743895.1191151.3772073.2536273.6024434.3984133z" />
-</svg>
-`;
-/* eslint-enable max-len */
+const icons = [];
+
+// ETH
+icons['0x0'] = eth;
+// SNGLS
+icons['0xaec2e87e0a235266d9c5adc9deb4b2e29b54d009'] = sngls;
+// SNGX
+icons['0x78774d1c3277b83459c730921bff11019017b233'] = sngx;
+// OMG
+icons['0xd26114cd6ee289accf82350c8d8487fedb8a0c07'] = omg;
+// GRMTK
+icons['0x1db42ceba6bdebf86d287e11b159ff97b083b8af'] = grmtk;
+// HAPPY
+icons['0x57889bb80ba8635159d1b30cd6c117337072dda7'] = happy;
+// QTUM
+icons['0x9a642d6b3368ddc662ca244badf32cda716005bc'] = qtum;
+// EOS
+icons['0x86fa049857e0209aa7d9e616f7eb3b3b78ecfdb0'] = eos;
+// MKR
+icons['0xc66ea802717bfb9833400264dd12c2bceaa34a6d'] = mkr;
+// REP
+icons['0xe94327d07fc17907b4db788e5adf2ed424addff6'] = rep;
+// GNT
+icons['0xa74476443119a942de498590fe1f2454d7d4ac0d'] = gnt;
+// PAY
+icons['0xb97048628db6b661d4c2aa833e95dbe1a905b280'] = pay;
+// DGD
+icons['0xe0b7927c4af23765cb51314a0e0521a9645f0e2a'] = dgd;
+// KNC
+icons['0xdd974d5c2e2928dea5f71b9825b8b646686bd200'] = knc;
+// BAT
+icons['0x0d8775f648430679a709e98d2b0cb6250d2887ef'] = bat;
+// ICN
+icons['0x888666ca69e0f178ded6d75b5726cee99a87d698'] = icn;
+// ZRX
+icons['0xe41d2489571d322189246dafa5ebde1f4699f498'] = zrx;
+// CVC
+icons['0x41e5560054824ea6b0732e656e3ad64e20e94e45'] = cvc;
+// SNT
+icons['0x744d70fdbe2ba4cf95131626614a1763df805b9e'] = snt;
+// MTL
+icons['0xf433089366899d83a9f26a773d59ec7ecf30355e'] = mtl;
+// ETHOS
+icons['0x5af2be193a6abca9c8817001f45744777db30756'] = ethos;
+// BNT
+icons['0x1f573d6fb3f13d689ff844b4ce37794d79a7ff1c'] = bnt;
+// GNO
+icons['0x6810e776880c02933d47db1b9fc05908e5386b96'] = gno;
+// MCO
+icons['0xb63b606ac810a52cca15e44bb630fd42d8d1d83d'] = mco;
+// FUN
+icons['0x419d0d8bdd9af5e606ae2232ed285aff190e711b'] = fun;
+// STORJ
+icons['0xb64ef51c888972c908cfacf59b47c1afbc0ab8ac'] = storj;
+// EDG
+icons['0x08711d3b02c8758f2fb3ab4e80228418a7f8e39c'] = edg;
+// MLN
+icons['0xbeb9ef514a379b997e0798fdcc901ee474b6d9a1'] = mln;
+// ANT
+icons['0x960b236a07cf122663c4303350609a66a7b288c0'] = ant;
+// WINGS
+icons['0x667088b212ce3d06a1b553a7221e1fd19000d9af'] = wings;
+// RLC
+icons['0x607f4c5bb672230e8672085532f7e901544a7375'] = rlc;
+// TAAS
+icons['0xe7775a6e9bcf904eb39da2b68c5efb4f9360e08c'] = taas;
+// AST
+icons['0x27054b13b1b798b345b591a4d22e6562d47ea75a'] = ast;
+// TKN
+icons['0xaaaf91d9b90df800df4f55c205fd6989c977e73a'] = tkn;
+// CFI
+icons['0x12fef5e57bf45873cd9b62e9dbd7bfb99e32d73e'] = cfi;
+// ST
+icons['0xaf30d2a7e90d7dc361c8c4585e9bb7d2f6f15bc7'] = st;
+// MANA
+icons['0x0f5d2fb29fb7d3cfee444a200298f468908cc942'] = mana;
+// TRST
+icons['0xcb94be6f13a1182e4a4b6140cb7bf2025d28e41b'] = trst;
+// DNT
+icons['0x0abdace70d3790235af448c88547603b945604ea'] = dnt;
+// XAURUM
+icons['0x4df812f6064def1e5e029f1ca858777cc98d2d81'] = xaurum;
+// ROL
+icons['0x2e071d2966aa7d8decb1005885ba1977d6038a65'] = rol;
+// SAN
+icons['0x7c5a0ce9267ed19b22f8cae653f198e3e8daf098'] = san;
+// TIME
+icons['0x6531f133e6deebe7f2dce5a0441aa7ef330b4e53'] = time;
+// NMR
+icons['0x1776e1f26f98b1a5df9cd347953a26dd3cb46671'] = nmr;
+// HMQ
+icons['0xcbcc0f036ed4788f63fc0fee32873d6a7487b908'] = hmq;
+// GUPPY
+icons['0xf7b098298f7c69fc14610bf71d5e02c60792894c'] = guppy;
+// ADT
+icons['0xd0d6d6c5fe4a677d343cc433536bb717bae167dd'] = adt;
+// LUN
+icons['0xfa05a73ffe78ef8f1a739473e462c54bae6567d9'] = lun;
+// NET
+icons['0xcfb98637bcae43c13323eaa1731ced2b716962fd'] = net;
+// BCAP
+icons['0xff3519eeeea3e76f1f699ccce5e23ee0bdda41ac'] = bcap;
+// SWT
+icons['0xb9e7f8568e08d5659f5d29c4997173d84cdf2607'] = swt;
+// PLU
+icons['0xd8912c10681d8b21fd3742244f44658dba12264e'] = plu;
+// VSLICE
+icons['0x5c543e7ae0a1104f78406c340e9c64fd9fce5170'] = vslice;
 
 export default icons;
